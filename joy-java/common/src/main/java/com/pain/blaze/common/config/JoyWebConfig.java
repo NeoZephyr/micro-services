6167b010c4894aba322dc0be0539ca8ffc1cd2fa
meilb <meimlb@163.com>
2021-02-21 23:19:32 +0800

    refactor

--- joy-java/common/src/main/java/com/pain/blaze/common/config/JoyWebConfig.java
+++
@@ -1,9 +0,0 @@
-package com.pain.blaze.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-@Configuration
-@Import(value = {JoyConfig.class})
-public class JoyWebConfig {
-}
--- joy-java/common/src/main/java/com/pain/blaze/common/env/EnvConstant.java
+++
@@ -1,8 +0,0 @@
-package com.pain.blaze.common.env;
-
-public interface EnvConstant {
-    String ENV_DEV = "dev";
-    String ENV_TEST = "test";
-    String ENV_UTA = "uta";
-    String ENV_PROD = "prod";
-}
