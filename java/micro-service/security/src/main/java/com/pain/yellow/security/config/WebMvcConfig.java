c9a062d618e9fe01ab422d163d735f43ce118beb
meilb <meimlb@163.com>
2021-03-21 10:18:47 +0800

    update

--- java/micro-service/security/src/main/java/com/pain/yellow/security/config/WebMvcConfig.java
+++ java/micro-service/security/src/main/java/com/pain/yellow/security/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.pain.yellow.security.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.passay.MessageResolver;
 import org.passay.spring.SpringMessageResolver;
@@ -7,22 +8,28 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.zalando.problem.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private final Environment environment;
     private final MessageSource messageSource;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
-                .addResourceHandler("/webjars/**")
-                .addResourceLocations("/webjars/")
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/")
                 .resourceChain(false);
         registry.setOrder(1);
     }
@@ -35,11 +42,44 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 
+//    /**
+//     * 配置 CORS
+//     * @param registry
+//     */
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        if (environment.acceptsProfiles(Profiles.of("dev"))) {
+//            registry
+//                    .addMapping("/**")
+//                    .allowedHeaders("*")
+//                    .exposedHeaders("X-Authenticate")
+//                    .allowedOrigins("http://localhost:4001");
+//        } else {
+//            registry
+//                    .addMapping("/**")
+//                    .allowedHeaders("*")
+//                    .exposedHeaders("X-Authenticate")
+//                    .allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS")
+//                    .allowedOrigins("https://uaa.imooc.com");
+//        }
+//    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().registerModules(
+                new ProblemModule(),
+                new ConstraintViolationProblemModule());
+    }
+
     @Bean
     public MessageResolver messageResolver() {
         return new SpringMessageResolver(messageSource);
     }
 
+    /**
+     * 配置 Java Validation 使用国际化的消息资源
+     * @return
+     */
     @Bean
     public LocalValidatorFactoryBean localValidatorFactoryBean() {
         LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
