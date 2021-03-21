3d0f292b557527f23e9f986c50aa82bb687fc85a
meilb <meimlb@163.com>
2021-03-21 10:56:37 +0800

    security

--- java/micro-service/security/src/main/java/com/pain/yellow/security/config/SecurityConfig.java
+++ java/micro-service/security/src/main/java/com/pain/yellow/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.pain.yellow.security.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pain.yellow.security.filter.JwtFilter;
 import com.pain.yellow.security.filter.RestAuthenticationFilter;
+import com.pain.yellow.security.service.impl.PasswordService;
 import com.pain.yellow.security.service.impl.UserService;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -30,9 +33,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,16 +51,18 @@ import java.util.Map;
 @Import(SecurityProblemSupport.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final Environment environment;
     private final ObjectMapper objectMapper;
     private final SecurityProblemSupport securityProblemSupport;
     private final DataSource dataSource;
     private final UserService userService;
+    private final PasswordService passwordService;
     private final JwtFilter jwtFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .requestMatchers(req -> req.mvcMatchers("/users/**", "/admin/**", "/authorize/**"))
+                .requestMatchers(req -> req.mvcMatchers("/users/**", "/authorize/**"))
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(exp -> exp
                         .authenticationEntryPoint(securityProblemSupport)
@@ -66,21 +76,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterAt(restAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // 配置跨域
                 .formLogin(form -> form.disable())
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .logout(logout -> logout.disable());
+
+        // TODO disable basic
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web
                 .ignoring()
-                .mvcMatchers("/public/**", "/error/**", "/h2-console/**");
+                .mvcMatchers("/resources/**", "/static/**", "/public/**", "/h2-console/**", "/error/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(userService)
+                .userDetailsService(userService) // 配置 AuthenticationManager 使用 userService
+                .userDetailsPasswordManager(passwordService) // 密码自动升级服务
                 .passwordEncoder(passwordEncoder());
 
 //        auth
@@ -110,6 +126,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         map.put(defaultId, new BCryptPasswordEncoder());
         map.put("SHA-1", new MessageDigestPasswordEncoder("SHA-1"));
         return new DelegatingPasswordEncoder(defaultId, map);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // 允许跨域访问的主机
+        if (environment.acceptsProfiles(Profiles.of("dev"))) {
+            configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4001"));
+        } else {
+            configuration.setAllowedOrigins(Collections.singletonList("https://uaa.imooc.com"));
+        }
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.addExposedHeader("X-Authenticate");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     private RestAuthenticationFilter restAuthenticationFilter() throws Exception {
