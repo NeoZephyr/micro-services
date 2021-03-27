6b01e091ad59e00b95564d69d0f070f9df3775e5
meilb <meimlb@163.com>
2021-03-27 19:29:47 +0800

    dev api

---
+++ java/micro-service/dev-api/src/main/java/com/pain/yellow/api/DevApiApplication.java
@@ -0,0 +1,13 @@
+package com.pain.yellow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevApiApplication.class, args);
+    }
+
+}
