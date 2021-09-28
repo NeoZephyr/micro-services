443f2912d92cc19b000e7c58230ba666e755c970
meilb <meimlb@163.com>
2021-09-29 00:28:28 +0800

    feat: 增加组件

---
+++ whale/wiki/web/src/components/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Footer...
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'TheFooter'
+});
+</script>
+
+<style scoped>
+
+</style>
